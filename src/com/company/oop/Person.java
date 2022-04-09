6eba9a5dd2c2d737fcafc61d098b0e69396eec6b
ingaj114 <ingaj114@gmail.com>
2022-04-10 00:45:49 +0300

    more on oop learning

--- src/com/company/oop/Person.java
+++ src/com/company/oop/Person.java
@@ -2,11 +2,12 @@ package com.company.oop;
 
 public class Person {
 
+
     //Define the properties or attributes of a person
     private String name;
     private int age;
 
-    //Constructor - a constructor is a utility that allows us to
+    // Constructor - a constructor is a utility that allows us to
     // create instances of objects
 
     // Method/Constructor overloading -> this is a way to define multiple constructors or methods
@@ -15,28 +16,17 @@ public class Person {
     // Method/Constructor signatures -> this is the number and data type of the parameter and
     // the order in which those parameters are defined
 
-/*    public Person() {
-    }*/
-
-/*    public Person(String name, int age) {
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
-    }*/
+    }
+
     // getters - allows us read data from the class
     // setters - allows us write a data to the class
 
-    /*    public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
         public int getAge() {
             return age;
         }
-        public void setAge() {
-            this.age = age;
-        }*/
     private double weight;
     private double height;
 
@@ -47,15 +37,15 @@ public class Person {
     public double getWeight() {
         return weight;
     }
-    /*    public void setWeight() {
+        public void setWeight() {
             this.weight = weight;
-        }*/
+        }
     public double getHeight() {
         return height;
     }
-    /*    public void setHeight() {
+       public void setHeight() {
             this.height = height;
-        }*/
+        }
     public double Result() {
         return weight/Math.pow(height,2);
     }
