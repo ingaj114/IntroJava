82dee48e0446404100ec76b5c5cbccdafcbef481
Ingaj114 <ingaj114@gmail.com>
2022-04-11 09:52:43 +0300

    oop assign

--- src/com/company/oop/assign/Company.java
+++ src/com/company/oop/assign/Company.java
@@ -1,16 +1,6 @@
 package com.company.oop.assign;
 
-public class Company extends Employee {
-    private String name;
-    private int age;
-    private long companyID;
-    private String companyName;
-
-    public Company(String name, int age) {
-        super(name, age);
-        this.name = name;
-        this.age = age;
-    }
+public class Company {
 
     public static void main(String[] args) {
         Person[] people = new Person[]{
@@ -20,6 +10,8 @@ public class Company extends Employee {
                 new Person("Lele", 45),
                 new Person("Olaf", 39),
         };
-        System.out.println("Average age of employees is " + getAveAge(people));
+
+        System.out.println("Average age of employees is " + Employee.getAveAge(people));
+
     }
 }
